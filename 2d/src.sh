7894da3df77f5b57eca765df94c98f48e09c657d
Matthew Martineau <m.martineau@hotmail.co.uk>
2015-10-14 11:29:34 +0100

    Updated Makefile with better defaults

--- 2d/src.sh
+++ 2d/src.sh
@@ -3,4 +3,4 @@
 source mod.src
 #source /cm/shared/languages/Intel-Compiler-XE-16/compilers_and_libraries_2016.0.109/linux/bin/compilervars.sh intel64
 source /cm/shared/languages/Intel-Compiler-XE-15_Update_2/composer_xe_2015.2.164/bin/compilervars.sh intel64
-#source /cm/shared/languages/Intel-Compiler-XE-15_Update_2/impi/5.0.3.048/mic/bin/mpivars.sh
+source /cm/shared/languages/Intel-Compiler-XE-15_Update_2/impi/5.0.3.048/mic/bin/mpivars.sh
