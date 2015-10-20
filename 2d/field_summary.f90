d9aa23e8dc33b5b11dbb4115461b12c59b502112
Matthew Martineau <m.martineau@hotmail.co.uk>
2015-10-20 14:14:31 -0500

    Fixing up field summary test cases

--- 2d/field_summary.f90
+++ 2d/field_summary.f90
@@ -26,136 +26,142 @@
 
 SUBROUTINE field_summary()
 
-    USE tea_module
-    USE field_summary_kernel_module
+USE tea_module
+USE field_summary_kernel_module
 
-    IMPLICIT NONE
+IMPLICIT NONE
 
-    INTEGER      :: c
-    REAL(KIND=8) :: vol,mass,ie,temp
-    REAL(KIND=8) :: kernel_time,timer
-    REAL(KIND=8) :: qa_diff
+INTEGER      :: c
+REAL(KIND=8) :: vol,mass,ie,temp
+REAL(KIND=8) :: kernel_time,timer
+REAL(KIND=8) :: qa_diff
 
-    !$ INTEGER :: OMP_GET_THREAD_NUM
+!$ INTEGER :: OMP_GET_THREAD_NUM
 
-    IF(parallel%boss)THEN
-        WRITE(g_out,*)
-        WRITE(g_out,*) 'Time ',time
-        WRITE(g_out,'(a13,5a16)')'           ','Volume','Mass','Density'       &
-            ,'Energy','U'
+IF(parallel%boss)THEN
+    WRITE(g_out,*)
+    WRITE(g_out,*) 'Time ',time
+    WRITE(g_out,'(a13,5a16)')'           ','Volume','Mass','Density'       &
+    ,'Energy','U'
+ENDIF
+
+IF(profiler_on) kernel_time=timer()
+
+IF(use_fortran_kernels)THEN
+    DO c=1,chunks_per_task
+    IF(chunks(c)%task.EQ.parallel%task) THEN
+        CALL field_summary_kernel(chunks(c)%field%x_min,                   &
+        chunks(c)%field%x_max,                   &
+        chunks(c)%field%y_min,                   &
+        chunks(c)%field%y_max,                   &
+        chunks(c)%field%volume,                  &
+        chunks(c)%field%density,                 &
+        chunks(c)%field%energy0,                 &
+        chunks(c)%field%u,                       &
+        vol,mass,ie,temp                         )
     ENDIF
-
-    IF(profiler_on) kernel_time=timer()
-
-    IF(use_fortran_kernels)THEN
-      DO c=1,chunks_per_task
-        IF(chunks(c)%task.EQ.parallel%task) THEN
-            CALL field_summary_kernel(chunks(c)%field%x_min,                   &
-                chunks(c)%field%x_max,                   &
-                chunks(c)%field%y_min,                   &
-                chunks(c)%field%y_max,                   &
-                chunks(c)%field%volume,                  &
-                chunks(c)%field%density,                 &
-                chunks(c)%field%energy0,                 &
-                chunks(c)%field%u,                       &
-                vol,mass,ie,temp                         )
-        ENDIF
-      ENDDO
+    ENDDO
     ELSEIF(use_ext_kernels) THEN
-      DO c=1,chunks_per_task
-        IF(chunks(c)%task.EQ.parallel%task) THEN
-            CALL ext_field_summary_kernel(c, vol, mass, ie, temp)
-        ENDIF
-      ENDDO
+    DO c=1,chunks_per_task
+    IF(chunks(c)%task.EQ.parallel%task) THEN
+        CALL ext_field_summary_kernel(c, vol, mass, ie, temp)
     ENDIF
+    ENDDO
+ENDIF
 
-    ! For mpi I need a reduction here
-    CALL tea_sum(vol)
-    CALL tea_sum(mass)
-    CALL tea_sum(ie)
-    CALL tea_sum(temp)
-    IF(profiler_on) profiler%summary=profiler%summary+(timer()-kernel_time)
+! For mpi I need a reduction here
+CALL tea_sum(vol)
+CALL tea_sum(mass)
+CALL tea_sum(ie)
+CALL tea_sum(temp)
+IF(profiler_on) profiler%summary=profiler%summary+(timer()-kernel_time)
 
+IF(parallel%boss) THEN
+    !$ IF(OMP_GET_THREAD_NUM().EQ.0) THEN
+    WRITE(g_out,'(a6,i7,5e16.7)')' step:',step,vol,mass,mass/vol,ie,temp
+    WRITE(g_out,*)
+    !$ ENDIF
+ENDIF
+
+!Check if this is the final call and if it is a test problem, check the result.
+IF(complete) THEN
     IF(parallel%boss) THEN
         !$ IF(OMP_GET_THREAD_NUM().EQ.0) THEN
-        WRITE(g_out,'(a6,i7,5e16.7)')' step:',step,vol,mass,mass/vol,ie,temp
-        WRITE(g_out,*)
+        IF(test_problem.GE.1) THEN
+
+            ! Note that the "correct" solution is with IEEE switched on, 1 task, 1 thread, Intel compiler on Ivy Bridge
+            IF(test_problem.EQ.1) qa_diff=ABS((100.0_8*(temp/157.550841832793_8))-100.0_8)
+            IF(test_problem.EQ.2) qa_diff=ABS((100.0_8*(temp/116.067951160930_8))-100.0_8)
+            IF(test_problem.EQ.3) qa_diff=ABS((100.0_8*(temp/95.4865103390698_8))-100.0_8)
+            IF(test_problem.EQ.4) qa_diff=ABS((100.0_8*(temp/166.838315378708_8))-100.0_8)
+            IF(test_problem.EQ.5) qa_diff=ABS((100.0_8*(temp/116.482111627676_8))-100.0_8)
+            IF(test_problem.EQ.6) qa_diff=ABS((100.0_8*(temp/103.88639125996923_8))-100.0_8)
+
+            ! Test cases calculated without IEEE compliance
+
+            ! 32x32
+            IF(test_problem.EQ.7) qa_diff=ABS((100.0_8*(temp/177.00175203427580_8))-100.0_8)
+
+            ! 64x64
+            IF(test_problem.EQ.8) qa_diff=ABS((100.0_8*(temp/139.97102840522848_8))-100.0_8)
+
+            ! 128x128
+            IF(test_problem.EQ.9) qa_diff=ABS((100.0_8*(temp/118.33195538063963_8))-100.0_8)
+
+            ! 256x256
+            IF(test_problem.EQ.10) qa_diff=ABS((100.0_8*(temp/108.12687625327908_8))-100.0_8)
+
+            ! 512x512
+            IF(test_problem.EQ.11) qa_diff=ABS((100.0_8*(temp/103.46970921076068_8))-100.0_8)
+
+            ! 1024x1024
+            IF(test_problem.EQ.12) qa_diff=ABS((100.0_8*(temp/101.21009327612924_8))-100.0_8)
+
+            ! 2048x2048
+            IF(test_problem.EQ.13) qa_diff=ABS((100.0_8*(temp/100.16633923123922_8))-100.0_8)
+
+            ! 4096x4096
+            IF(test_problem.EQ.14) qa_diff=ABS((100.0_8*(temp/99.695059768963887_8))-100.0_8)
+
+            ! 4000 4000
+            IF(test_problem.EQ.15) qa_diff=ABS((100.0_8*(temp/99.756584330021866_8))-100.0_8)
+
+            ! 316x316
+            IF(test_problem.EQ.16) qa_diff=ABS((100.0_8*(temp/106.22536751207716_8))-100.0_8)
+
+            ! 447x447
+            IF(test_problem.EQ.17) qa_diff=ABS((100.0_8*(temp/103.75236286020100_8))-100.0_8)
+
+            ! 548x548
+            IF(test_problem.EQ.18) qa_diff=ABS((100.0_8*(temp/102.95322957172701_8))-100.0_8)
+
+            ! 632x632
+            IF(test_problem.EQ.19) qa_diff=ABS((100.0_8*(temp/102.61257379535277_8))-100.0_8)
+
+            ! 707x707
+            IF(test_problem.EQ.20) qa_diff=ABS((100.0_8*(temp/101.99792927438858_8))-100.0_8)
+
+            ! 775x775
+            IF(test_problem.EQ.21) qa_diff=ABS((100.0_8*(temp/101.93825682423312_8))-100.0_8)
+
+            ! 837x837
+            IF(test_problem.EQ.22) qa_diff=ABS((100.0_8*(temp/101.54636465546844_8))-100.0_8)
+
+            ! 894x894
+            IF(test_problem.EQ.23) qa_diff=ABS((100.0_8*(temp/101.51302189154072_8))-100.0_8)
+
+            WRITE(*,'(a,i4,a,f16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
+            WRITE(g_out,'(a,i4,a,e16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
+            IF(qa_diff.LT.0.001) THEN
+                WRITE(*,*)"This test is considered "//achar(27)//"[32m PASSED"//achar(27)//"[0m."
+                WRITE(g_out,*)"This test is considered PASSED"
+            ELSE
+                WRITE(*,*)"This test is considered "//achar(27)//"[31m NOT PASSED"//achar(27)//"[0m."
+                WRITE(g_out,*)"This test is considered NOT PASSED"
+            ENDIF
+        ENDIF
         !$ ENDIF
     ENDIF
-
-    !Check if this is the final call and if it is a test problem, check the result.
-    IF(complete) THEN
-        IF(parallel%boss) THEN
-            !$ IF(OMP_GET_THREAD_NUM().EQ.0) THEN
-            IF(test_problem.GE.1) THEN
-
-                IF(test_problem.EQ.6) qa_diff=ABS((100.0_8*(temp/103.88639125996923_8))-100.0_8) ! 500x500 20 steps
-
-                ! 32x32
-                IF(test_problem.EQ.7) qa_diff=ABS((100.0_8*(temp/177.00175203427580_8))-100.0_8)
-
-                ! 64x64
-                IF(test_problem.EQ.8) qa_diff=ABS((100.0_8*(temp/139.97102840522848_8))-100.0_8)
-
-                ! 128x128
-                IF(test_problem.EQ.9) qa_diff=ABS((100.0_8*(temp/118.33195538063963_8))-100.0_8)
-
-                ! 256x256
-                IF(test_problem.EQ.10) qa_diff=ABS((100.0_8*(temp/108.12687625327908_8))-100.0_8)
-
-                ! 512x512
-                IF(test_problem.EQ.11) qa_diff=ABS((100.0_8*(temp/103.46970921076068_8))-100.0_8)
-
-                ! 1024x1024
-                IF(test_problem.EQ.12) qa_diff=ABS((100.0_8*(temp/101.21009327612924_8))-100.0_8)
-
-                ! 2048x2048
-                IF(test_problem.EQ.13) qa_diff=ABS((100.0_8*(temp/100.16633923123922_8))-100.0_8)
-
-                ! 4096x4096
-                IF(test_problem.EQ.14) qa_diff=ABS((100.0_8*(temp/99.695059768963887_8))-100.0_8)
-
-                ! 4000 4000
-                IF(test_problem.EQ.15) qa_diff=ABS((100.0_8*(temp/99.756584330021866_8))-100.0_8)
-
-                ! 316x316
-                IF(test_problem.EQ.16) qa_diff=ABS((100.0_8*(temp/106.22536751207716_8))-100.0_8)
-
-                ! 447x447
-                IF(test_problem.EQ.17) qa_diff=ABS((100.0_8*(temp/103.75236286020100_8))-100.0_8)
-
-                ! 548x548
-                IF(test_problem.EQ.18) qa_diff=ABS((100.0_8*(temp/102.95322957172701_8))-100.0_8)
-
-                ! 632x632
-                IF(test_problem.EQ.19) qa_diff=ABS((100.0_8*(temp/102.61257379535277_8))-100.0_8)
-
-                ! 707x707
-                IF(test_problem.EQ.20) qa_diff=ABS((100.0_8*(temp/101.99792927438858_8))-100.0_8)
-
-                ! 775x775
-                IF(test_problem.EQ.21) qa_diff=ABS((100.0_8*(temp/101.93825682423312_8))-100.0_8)
-
-                ! 837x837
-                IF(test_problem.EQ.22) qa_diff=ABS((100.0_8*(temp/101.54636465546844_8))-100.0_8)
-
-                ! 894x894
-                IF(test_problem.EQ.23) qa_diff=ABS((100.0_8*(temp/101.51302189154072_8))-100.0_8)
-
-
-
-               WRITE(*,'(a,i4,a,f16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
-                WRITE(g_out,'(a,i4,a,e16.7,a)')"Test problem", Test_problem," is within",qa_diff,"% of the expected solution"
-                IF(qa_diff.LT.0.001) THEN
-                    WRITE(*,*)"This test is considered "//achar(27)//"[32m PASSED"//achar(27)//"[0m."
-                    WRITE(g_out,*)"This test is considered PASSED"
-                ELSE
-                    WRITE(*,*)"This test is considered "//achar(27)//"[31m NOT PASSED"//achar(27)//"[0m."
-                    WRITE(g_out,*)"This test is considered NOT PASSED"
-                ENDIF
-            ENDIF
-            !$ ENDIF
-        ENDIF
-    ENDIF
+ENDIF
 
 END SUBROUTINE field_summary
